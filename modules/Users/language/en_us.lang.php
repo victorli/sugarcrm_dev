db65728f15aa79ca18ce4c88fce7b5cc41a77499
victorli <luckylzs@gmail.com>
2016-06-02 22:35:19 +0800

    Update en_us.lang.php

--- modules/Users/language/en_us.lang.php
+++ modules/Users/language/en_us.lang.php
@@ -98,7 +98,7 @@ $mod_strings = array (
 	'LBL_ADDRESS_STREET'				=> 'Address Street',
 	'LBL_ADDRESS'						=> 'Address',
 	'LBL_ADMIN_USER'					=> 'System Administrator User',
-	'LBL_TENANT_USRE'					=>	'Tenant User',
+	'LBL_TENANT_USER'					=>	'Tenant User',
 	'LBL_TENANT_DESC'					=>	'Tenant User',
 
 	'LBL_ADMIN_DESC'					=> 'User can access the Administration page all records.',
