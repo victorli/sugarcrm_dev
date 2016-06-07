406f5195319c79a460188787b3e2905e60de420b
victorli <luckylzs@gmail.com>
2016-06-07 15:09:35 +0800

    added tenant_id field

--- modules/Notes/vardefs.php
+++ modules/Notes/vardefs.php
@@ -51,6 +51,14 @@ $dictionary['Note'] = array(
     'reportable'=>true,
     'comment' => 'Unique identifier'
   ),
+  'tenant_id' => array(
+  	'name' => 'tenant_id',
+  	'vname' => 'LBL_TENANT_ID',
+  	'type' => 'id',
+  	'required' => true,
+  	'default'  => '1',
+  	'comment'  => 'the tenant id of this record creator'
+ ),
    'date_entered' =>
   array (
     'name' => 'date_entered',
