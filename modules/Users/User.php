103140fed895f22bd084a834935cde4ff18afe2c
victorli <luckylzs@gmail.com>
2016-06-10 22:15:17 +0800

    Update User.php

--- modules/Users/User.php
+++ modules/Users/User.php
@@ -1604,19 +1604,19 @@ EOQ;
    function create_new_list_query($order_by, $where,$filter=array(),$params=array(), $show_deleted = 0,$join_type='', $return_array = false,$parentbean=null, $singleSelect = false)
    {	
    	//added tenant self in array
-   	/*if(!is_sys_admin($GLOBALS['current_user'])){
+   	if(!is_sys_admin($GLOBALS['current_user'])){
    		if(is_tenant($GLOBALS['current_user'])){
    			if(empty($where))
-   				$where =" 1=1 OR $this->table_name.id='".$GLOBALS['current_user']->id."' ";
+   				$where =" ($this->table_name.id='".$GLOBALS['current_user']->id."' OR $this->table_name.tenant_id='".$GLOBALS['current_user']->id."') ";
    			else
-   				$where .=" OR $this->table_name.id='".$GLOBALS['current_user']->id."' ";	
+   				$where .=" AND ($this->table_name.id='".$GLOBALS['current_user']->id."' OR $this->table_name.tenant_id='".$GLOBALS['current_user']->id."') ";	
    		}else{
    			if(empty($where))
-   				$where =" 1=1 OR $this->table_name.id='".$GLOBALS['current_user']->tenant_id."' ";
+   				$where =" ($this->table_name.id='".$GLOBALS['current_user']->tenant_id."' OR $this->table_name.tenant_id='".$GLOBALS['current_user']->tenant_id."') ";
    			else
-   				$where .=" OR $this->table_name.id='".$GLOBALS['current_user']->tenant_id."' ";	
+   				$where .=" AND ($this->table_name.id='".$GLOBALS['current_user']->tenant_id."' OR $this->table_name.tenant_id='".$GLOBALS['current_user']->tenant_id."') ";	
    		}
-   	}*/
+   	}
    	//call parent method, specifying for array to be returned
    	$ret_array = parent::create_new_list_query($order_by, $where,$filter,$params, $show_deleted,$join_type, true,$parentbean, $singleSelect);
 
