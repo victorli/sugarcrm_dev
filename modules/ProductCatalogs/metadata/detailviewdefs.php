1d06107157b8beb299cbe557cbe75779ed60be1e
victorli <luckylzs@gmail.com>
2016-06-23 15:02:57 +0800

    Update detailviewdefs.php

--- modules/ProductCatalogs/metadata/detailviewdefs.php
+++ modules/ProductCatalogs/metadata/detailviewdefs.php
@@ -17,3 +17,37 @@
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  @date  2016-6-21
 */
+$viewdefs['ProductCatalogs']['DetailView'] = array(
+	'templateMeta' => array(
+		'form' => array(
+			'buttons'=>array(
+				'EDIT',
+				'DELETE',
+				),
+			),
+		'maxColumns' => '2',
+		'widths' => array(
+			array('label'=>10,'field'=>30),
+			array('label'=>'10','field'=>'30'),
+			),
+		'includes' => array(),
+	),
+	'panels' => array(
+		'LBL_PRODUCT_CATALOG_INFORMATION' => array(
+			array(
+				array('name' =>'name','label' => 'LBL_SUBJECT'),	
+			),
+			array(
+				array('name' => 'parent_id','label' => 'LBL_PARENT_NAME','customCode'=>'parent name'),
+				array('name' => 'visible','label' => 'LBL_VISIBLE')
+			),
+			array(
+				array('name'=>'cover_image','type'=>'photo')
+				array('name'=>'thumbnail','type'=>'photo')
+			),
+			array(
+			  'description',  
+			),
+		),
+	),
+);
